792c72461dd84b8dc90912bc3919242c50cb4476
Hemanthnemili <hemanth.nemili@gmail.com>
2024-01-04 16:01:18 +0530

    basic route structure done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDb from "./database/db.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 
 dotenv.config();
 
@@ -14,6 +16,11 @@ app.use(cors());
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/post", postRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port http://localhost:${PORT}`);
